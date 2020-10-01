c8cc39fb98bff04809955322bef1fd82c12b7fa4
Higor Denomar <higordenomar@hotmail.com>
2020-10-01 16:02:06 -0300

    documented App.js

--- mobile/App.js
+++ mobile/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Routes from './src/routes';
 import { StatusBar, YellowBox } from 'react-native';
 
+// Ignorando aviso no App do Expo.
 YellowBox.ignoreWarnings([
   'Unrecognized WebSocket'
 ]);
 
 export default function App() {
-  return (
 
+  // Modificando a StatusBar e chamando o componente de Rotas.
+  return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7d40e7"/>
-      <Routes/>
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+      <Routes />
     </>
   );
 }
